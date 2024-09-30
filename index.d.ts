b38f80f583bb847532e940bfbec21993320ed268
alexy-os <agorbunoff.ru@gmail.com>
2024-09-30 16:31:03 +0300

    renew from rollup

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,9 @@
-import Handlebars from 'handlebars';
-
-interface RenderContext {
-    [key: string]: any;
-}
-declare function render(template: string, context?: RenderContext, layout?: string): Promise<string>;
-declare function clearCache(): void;
-declare function registerHelper(name: string, helper: Handlebars.HelperDelegate): void;
-declare function setCaching(enabled: boolean): void;
-declare function setCacheTTL(ttl: number): void;
-declare function initHandlebars(): Promise<void>;
-
 /**
- * Configuration interface for the application.
- *
- * @property {string} VIEWS_DIR - Directory path for views.
- * @property {string} PARTIALS_DIR - Directory path for partials.
- * @property {string} LAYOUTS_DIR - Directory path for layouts.
- * @property {'debug' | 'info' | 'warn' | 'error'} LOG_LEVEL - Logging level for the application.
+ * Exporting functions from engine module
  */
-interface Config {
-    VIEWS_DIR: string;
-    PARTIALS_DIR: string;
-    LAYOUTS_DIR: string;
-    LOG_LEVEL: 'debug' | 'info' | 'warn' | 'error';
-}
+export { render, clearCache, registerHelper, setCaching, setCacheTTL, initHandlebars } from './engine';
 /**
- * Sets a new configuration for the application.
- *
- * @param {Partial<Config>} newConfig - Partial configuration to be merged with the existing one.
+ * Exporting setConfig function and Config type from config module
  */
-declare function setConfig(newConfig: Partial<Config>): void;
-
-export { type Config, clearCache, initHandlebars, registerHelper, render, setCacheTTL, setCaching, setConfig };
+export { setConfig } from './config';
+export type { Config } from './config';
